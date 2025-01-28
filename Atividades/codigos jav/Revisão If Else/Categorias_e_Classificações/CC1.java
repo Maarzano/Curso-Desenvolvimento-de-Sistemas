9661fbe415ac9def1be03ea8be6836174d20a65b
Marzano <arthuramarzano10@gmail.com>
2025-01-28 14:39:28 -0300

    iniciando CC

---
+++ Atividades/codigos jav/Revisão If Else/Categorias_e_Classificações/CC1.java
@@ -0,0 +1,10 @@
+package Categorias_e_Classificações;
+
+import java.util.Scanner;
+
+public class CC1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
