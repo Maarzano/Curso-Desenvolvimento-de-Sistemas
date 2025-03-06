7c675b589ab0600b574e9302fa35cf955e5524a9
Marzano <arthuramarzano10@gmail.com>
2025-03-06 07:21:35 -0300

    exercício 1 pronto

---
+++ Atividades/codigos jav/exercícioFixação/3exercícios/Att1/Models/Rectangle.java
@@ -0,0 +1,21 @@
+package Models;
+
+public class Rectangle {
+    private double Width;
+    private double Height;
+
+    public Rectangle(double width, double height){
+        this.Width = width;
+        this.Height = height;
+    }
+    public double Area(){
+        return Width * Height; 
+    }
+    public double Perimeter(){
+        return 2 * (Width + Height);
+    }
+    public double Diagonal(){
+        return Math.sqrt(Math.pow(Width, 2) + Math.pow(Height, 2));
+    }
+
+}
---
+++ Atividades/codigos jav/exercícioFixação/3exercícios/Att1/Program.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+import Models.Rectangle;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o valor da altura do retângulo: ");
+        double altura = sc.nextDouble();
+        System.out.print("Agora digite o valor da largura do retângulo: ");
+        double largura = sc.nextDouble();
+
+        Rectangle retangulo = new Rectangle(altura, largura);
+
+        System.out.println("Area: " + retangulo.Area());
+        System.out.println("Permetro: " + retangulo.Perimeter());
+        System.out.println("Diagonal: " + retangulo.Diagonal());
+
+        
+    }
+}
