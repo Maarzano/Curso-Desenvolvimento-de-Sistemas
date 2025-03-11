dbf90fa6c06aef3e7c5d0c9d586247abfbe92123
Marzano <80858692+Maarzano@users.noreply.github.com>
2025-03-11 19:20:17 -0300

    Create Program.java

---
+++ Atividades/codigos jav/Construtores/Exercício1/Program.java
@@ -0,0 +1,7 @@
+public static void main(String[] args) {
+  
+        Carro meuCarro = new Carro("Toyota", 2020);
+
+        System.out.println("Marca: " + meuCarro.marca);
+        System.out.println("Ano: " + meuCarro.ano);
+}
